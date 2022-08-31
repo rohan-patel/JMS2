663020097b66b26c28439071af7b91fa9241bd26
Rohan Patel <82868610+rohan-patel@users.noreply.github.com>
2022-08-31 19:23:53 +0530

    request-reply-init

---
+++ jmsfundamentals/src/main/java/com/rohan/jms/messagestructure/RequestReplyDemo.java
@@ -0,0 +1,39 @@
+package com.rohan.jms.basics;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class RequestReplyDemo {
+
+	public static void main(String[] args) throws NamingException {
+		
+		InitialContext context = new InitialContext();
+		Queue queue = (Queue) context.lookup("queue/requestQueue");
+		Queue replyQueue = (Queue) context.lookup("queue/replyQueue");
+		
+		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext()) {
+			
+			JMSProducer producer = jmsContext.createProducer();
+			producer.send(queue, "Arise, Awake and Stop not till he goal is not reached");
+
+			JMSConsumer consumer = jmsContext.createConsumer(queue);
+			String messageReceived = consumer.receiveBody(String.class);
+			System.out.println(messageReceived);
+
+			JMSProducer replyProducer = jmsContext.createProducer();
+			replyProducer.send(replyQueue, "You are awesome!");
+
+			JMSConsumer replyConsumer = jmsContext.createConsumer(replyQueue);
+			System.out.println(replyConsumer.receiveBody(String.class));
+
+		}
+		
+	}
+
+}
