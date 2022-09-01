d0453b6aae9f614a51999d8d3657a7960e9e3c55
Rohan Patel <82868610+rohan-patel@users.noreply.github.com>
2022-09-01 14:46:14 +0530

    Message Types - Bytes

---
+++ jmsfundamentals/src/main/java/com/rohan/jms/messagestructure/MessageTypesDemo.java
@@ -0,0 +1,46 @@
+package com.rohan.jms.messagestructure;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
+import javax.jms.TextMessage;
+import javax.jms.BytesMessage;
+import javax.jms.Message;
+import javax.jms.JMSException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import java.util.Map;
+import java.util.HashMap;
+
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MessageTypesDemo {
+
+	public static void main(String[] args) throws NamingException, InterruptedException {
+		
+		InitialContext context = new InitialContext();
+		Queue queue = (Queue) context.lookup("queue/myQueue");
+		
+		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext()) {
+
+            JMSProducer producer = jmsContext.createProducer();
+			TextMessage message = jmsContext.createTextMessage("Arise, Awake and Stop not till he goal is not reached");
+			BytesMessage bytesMessage = jmsContext.createBytesMessage();
+			bytesMessage.writeUTF("Rohan");
+			bytesMessage.writeLong(123l);
+
+			producer.send(queue, bytesMessage);
+
+			BytesMessage messageReceived = (BytesMessage) jmsContext.createConsumer(queue).receive(5000);
+			System.out.println(messageReceived.readUTF());
+			System.out.println(messageReceived.readLong());
+
+			
+		}
+		
+    }
+}
