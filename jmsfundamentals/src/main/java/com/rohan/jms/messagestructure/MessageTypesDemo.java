6ee42a6fd1ac715e1aa7934a1fb6075f3900fb12
Rohan Patel <82868610+rohan-patel@users.noreply.github.com>
2022-09-01 15:22:03 +0530

    types of messages

--- jmsfundamentals/src/main/java/com/rohan/jms/messagestructure/MessageTypesDemo.java
+++ jmsfundamentals/src/main/java/com/rohan/jms/messagestructure/MessageTypesDemo.java
@@ -6,6 +6,8 @@ import javax.jms.JMSProducer;
 import javax.jms.Queue;
 import javax.jms.TextMessage;
 import javax.jms.BytesMessage;
+import javax.jms.StreamMessage;
+import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.JMSException;
 import javax.naming.InitialContext;
@@ -29,15 +31,33 @@ public class MessageTypesDemo {
 
             JMSProducer producer = jmsContext.createProducer();
 			TextMessage message = jmsContext.createTextMessage("Arise, Awake and Stop not till he goal is not reached");
+
 			BytesMessage bytesMessage = jmsContext.createBytesMessage();
 			bytesMessage.writeUTF("Rohan");
 			bytesMessage.writeLong(123l);
+			
+			StreamMessage streamMessage = jmsContext.createBytesMessage();
+			streamMessage.writeBoolean(true);
+			streamMessage.writeFloat(22.25f);
 
-			producer.send(queue, bytesMessage);
+			MapMessage mapMessage = jmsContext.createMapMessage();
+			mapMessage.setBoolean("IsCreditAvailable", true);
 
-			BytesMessage messageReceived = (BytesMessage) jmsContext.createConsumer(queue).receive(5000);
-			System.out.println(messageReceived.readUTF());
-			System.out.println(messageReceived.readLong());
+			producer.send(queue, mapMessage);
+
+			// System.out.println("<------ Bytes Message ------>");
+			// BytesMessage messageReceived = (BytesMessage) jmsContext.createConsumer(queue).receive(5000);
+			// System.out.println(messageReceived.readUTF());
+			// System.out.println(messageReceived.readLong());
+
+			// System.out.println("<------ Streams Message ------>");
+			// StreamMessage messageReceived = (StreamMessage) jmsContext.createConsumer(queue).receive(5000);
+			// System.out.println(messageReceived.readBoolean());
+			// System.out.println(messageReceived.readFloat());
+
+			System.out.println("<------ Maps Message ------>");
+			StreamMessage messageReceived = (StreamMessage) jmsContext.createConsumer(queue).receive(5000);
+			System.out.println(messageReceived.getBoolean());
 
 			
 		}
