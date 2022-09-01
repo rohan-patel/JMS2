2485c8393d9e992be3c7749b9e1aafe8a3e78b3d
Rohan Patel <82868610+rohan-patel@users.noreply.github.com>
2022-09-01 12:39:32 +0530

    adding custom message properties

---
+++ jmsfundamentals/src/main/java/com/rohan/jms/messagestructure/MessagePropertiesDemo.java
@@ -0,0 +1,45 @@
+package com.rohan.jms.messagestructure;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSProducer;
+import javax.jms.Queue;
+import javax.jms.TextMessage;
+import javax.jms.Message;
+import javax.jms.JMSException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import java.util.Map;
+import java.util.HashMap;
+
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MessageDelayDemo {
+
+	public static void main(String[] args) throws NamingException, InterruptedException {
+		
+		InitialContext context = new InitialContext();
+		Queue queue = (Queue) context.lookup("queue/myQueue");
+		
+		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext()) {
+
+            JMSProducer producer = jmsContext.createProducer();
+			TextMessage message = jmsContext.createTextMessage("Arise, Awake and Stop not till he goal is not reached");
+			message.setBooleanProperty("loggedIn", true);
+			message.setStringProperty("userToken", "132asd44dfe");
+			producer.send(queue, message);
+
+			Message messageReceived = jmsContext.createConsumer(queue).receive(5000);
+			System.out.println(messageReceived);
+			System.out.println(messageReceived.getBooleanProperty("loggedIn"));
+			System.out.println(messageReceived.getStringProperty("userToken"));
+
+
+			
+		}
+		
+    }
+}
